710151951fd18dd96be1fe7b0e9fe51bdfb00ded
Yogaraj <yoga30696@gmail.com>
2019-10-25 22:12:00 +0530

    added cors

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var postFetchScheduler = require('./schedules/postFetch_Scheduler');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var post = require('./routes/getPost');
+var postDetails = require('./routes/getPostDetails');
 
 var app = express();
+app.use(cors());
+
+app.options("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //other headers here
+  res.status(200).end();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,16 +37,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/getpost', post);
+app.use('/getPostDetails', postDetails);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
