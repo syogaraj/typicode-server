655705e14046de551383eef3fa655ef964f69f5c
Yogaraj <yoga30696@gmail.com>
2019-10-25 21:58:51 +0530

    scheduler - ordered

--- schedules/postFetch_Scheduler.js
+++ schedules/postFetch_Scheduler.js
@@ -7,34 +7,81 @@ let executor = require('../mysql_/mysqlExecutor');
 
 let postFetchURL = "https://jsonplaceholder.typicode.com/comments";
 
-function parseAndInsertComments(data) {
-    let insertPrefix = "insert ignore into Comments (POST_ID, ID, EMAIL, NAME, BODY) values ";
-    let insertValues = "";
-    let commentKeyOrder = ['postId', 'id', 'name', 'email', 'body'];
-    data.forEach((eachComment) => {
-
-        let tempInsValues = "";
-        let toInsert = true;
-        commentKeyOrder.forEach(function (key) {
-            if (eachComment.hasOwnProperty(key)) {
-                let val = JSON.stringify(eachComment[key]).replace(/\"/g, "");
-                tempInsValues += "'" + val + "',";
-            } else {
-                toInsert = false;
-                return;
-            }
-        });
-        if (toInsert) {
-            tempInsValues = tempInsValues.replace(/,(\s+)?$/g, "");
-            insertValues += "(";
-            insertValues += tempInsValues;
-            insertValues += "),";
-            insertValues = insertValues.replace(/,(\s+)?$/g, "")
+function getUserID(eachComment) {
+    let userOrder = ['name', 'email'];
+    let insertUserQuery = "insert ignore into UserDetails (NAME, EMAIL) values ";
+    let insertUserVal = "";
+    let tempUserVal = "";
+    let insertUser = true;
+    let userDetail = {}
+    userOrder.forEach(function (key) {
+        if (eachComment.hasOwnProperty(key)) {
+            let val = JSON.stringify(eachComment[key]).replace(/\"/g, "");
+            userDetail[key] = val;
+            tempUserVal += "'" + val + "',";
+        } else {
+            insertUser = false;
+            return;
         }
-
     });
-    console.log("Final Query:" + insertPrefix + insertValues);
-    console.log("-------------------");
+    if (insertUser) {
+        tempUserVal = tempUserVal.replace(/,(\s+)?$/g, "");
+        insertUserVal += "(";
+        insertUserVal += tempUserVal;
+        insertUserVal += "),";
+        insertUserVal = insertUserVal.replace(/,(\s+)?$/g, "")
+    }
+    // console.log(insertUserQuery + insertUserVal);
+    return executor.executeQuery(insertUserQuery + insertUserVal).then(function (response) {
+        let userIDQuery = "select ID from UserDetails where NAME='" + userDetail['name'] + "' and EMAIL = '" + userDetail['email'] + "'";
+        return executor.getResultAsString(userIDQuery);
+    }).catch(error => {
+        return null;
+    });
+}
+
+function insertPostDetails(eachComment, userID) {
+    let insertPrefix = "insert ignore into PostDetails (ID, POST_ID, BODY) values ";
+    let insertValues = "";
+    let commentKeyOrder = ['postId', 'body'];
+    let tempInsValues = "";
+    let toInsert = true;
+    commentKeyOrder.forEach(function (key) {
+        if (eachComment.hasOwnProperty(key)) {
+            let val = JSON.stringify(eachComment[key]).replace(/\"/g, "");
+            tempInsValues += "'" + val + "',";
+        } else {
+            toInsert = false;
+            return;
+        }
+    });
+    if (toInsert) {
+        tempInsValues = tempInsValues.replace(/,(\s+)?$/g, "");
+        insertValues += "(";
+        insertValues += userID + ",";
+        insertValues += tempInsValues;
+        insertValues += "),";
+        insertValues = insertValues.replace(/,(\s+)?$/g, "");
+    }
+    // console.log(insertPrefix + insertValues);
+    executor.executeQuery(insertPrefix + insertValues).then(success => {
+        // do nothing;
+    }).catch(error => {
+        console.log(error);
+    })
+}
+
+
+function parseAndInsertComments(data) {
+
+    data.forEach((eachComment) => {
+        let userID = "";
+        getUserID(eachComment).then(function (response) {
+            userID = response[0].ID;
+            // console.log("------" + userID);
+            insertPostDetails(eachComment, userID);
+        });
+    });
 }
 
 function scheduleJob() {
@@ -44,6 +91,7 @@ function scheduleJob() {
         axios.get(postFetchURL).then(function (response) {
             let commentData = response.data;
             parseAndInsertComments(commentData);
+            console.log("Job finished at - " + new Date());
         }).catch(function (error) {
             console.log(error);
         });
