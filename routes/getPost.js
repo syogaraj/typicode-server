1b3cb65740db3190afbf3adfd3724fe2ad15eabe
Yogaraj <yoga30696@gmail.com>
2019-10-25 22:01:43 +0530

    getTopPost view added

---
+++ routes/getPost.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var executor = require('../mysql_/mysqlExecutor');
+
+const topPostQuery = "select POST_ID, count(*) as COUNT from PostDetails group by POST_ID";
+
+router.get('/', function(req, res, next) {
+    executor.getResults(topPostQuery).then(result => {
+        res.send(result);
+    }).catch(error => {
+        res.status(500);
+    })
+});
+
+module.exports = router;
