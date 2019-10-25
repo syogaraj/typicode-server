0e7af28699d3ddbcf4eb1542ad222adcc1c55beb
Yogaraj <yoga30696@gmail.com>
2019-10-25 21:58:29 +0530

    updated PostDetails table query

--- migrations/migration.js
+++ migrations/migration.js
@@ -25,7 +25,7 @@ function createTables() {
         message => { console.log("Migration: PostDetails table already exists!") }
     ).catch(table_does_not_exist => {
         console.log("Comment table doesn't exists! Going to create");
-        const create_post_query = "CREATE TABLE `PostDetails` ( `ID` bigint(20) NOT NULL, `BODY` varchar(1000) DEFAULT NULL, KEY`ID`(`ID`), CONSTRAINT`PostDetails_ibfk_1` FOREIGN KEY(`ID`) REFERENCES`UserDetails`(`ID`))";
+        const create_post_query = "CREATE TABLE `PostDetails` (`ID` bigint(20) DEFAULT NULL, `BODY` varchar(1000) DEFAULT NULL, `POST_ID` bigint(20) DEFAULT NULL, KEY`ID`(`ID`), KEY`postID_INDEX`(`POST_ID`), CONSTRAINT`PostDetails_ibfk_1` FOREIGN KEY(`ID`) REFERENCES`UserDetails`(`ID`))";
         executor.executeQuery(create_post_query).then(
             message => { console.log("PostDetails table successfully created."); }
         ).catch(exception => {
